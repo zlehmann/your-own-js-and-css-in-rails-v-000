2b11574d6e9258f762a49b3ed75150d33de9dd2a
Carleton DiLeo <cbdileo@gmail.com>
2016-02-10 14:27:06 -0800

    Fixed specs

--- rails_app/spec/features/styles_spec.rb
+++ rails_app/spec/features/styles_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 feature "Page Styles" do
   scenario "'Click To Hide Me' link has error CSS class" do
     visit '/'
-    expect(page).to have_css("a error", text: "Click To Hide Me")
+    expect(page).to have_css("a.error", text: "Click To Hide Me")
   end
 
   scenario "body tag has cool-background CSS class" do
     visit '/'
-    expect(page).to have_css("body cool-background")
+    expect(page).to have_css("body.cool-background")
   end
 end
